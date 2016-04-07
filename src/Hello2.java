ba72bcbfac9c7b35ca690d8a084d21e777882b52
tmp <nana@yaaa.org>
2016-04-07 11:55:16 +0300

    test

--- src/Hello2.java
+++ src/Hello2.java
@@ -1,8 +1,8 @@
 /**
  * Created by tmp on 07.04.2016.
  */
-public class Hello {
+public class Hello2 {
     public static void main(String[] args) {
-        System.out.println("Hello World from home!");
+        System.out.println("Test Hello");
     }
 }
